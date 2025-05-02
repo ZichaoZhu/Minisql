1250dc4716315175cd8147143cc13ee563010745
ZichaoZhu <1606783699@qq.com>
2025-05-02 14:26:16 +0800

    完成row.h中的DeserializeFrom()函数的编写，以及修改了SerializeTo()中的一些代码

--- src/include/record/row.h
+++ src/include/record/row.h
@@ -88,7 +88,7 @@ class Row {
   uint32_t SerializeTo(char *buf, Schema *schema) const {
     ASSERT(schema != nullptr, "Invalid Schema");
     ASSERT(schema->GetColumnCount() == fields_.size(), "Invalid field");
-    unsigned offset = 0;  // 定义偏移量
+    uint32_t offset = 0;  // 定义偏移量
     size_t field_nums = fields_.size(); // 得到字段个数
     MACH_WRITE_UINT32(buf, field_nums); // 写入字段个数
     offset = sizeof(field_nums);
@@ -99,15 +99,15 @@ class Row {
      * 对于最后多出不满8个的字段，我们在后面补0
      * 也就是说，我们使用了单页的bitmap，组成了多页的bitmaps
      */
-    unsigned char_num = (field_nums + 7) / 8; // 计算需要多少个char类型变量
-    for (unsigned i = 0; i < char_num; i++) {
+    uint32_t bitmap_num = (field_nums + 7) / 8; // 计算需要多少个bitmap
+    for (uint32_t i = 0; i < bitmap_num; i++) {
       /* 此时，是第i个null_bitmap表被序列化
        * 我们首先计算null_bitmap表
        * 然后再将其写入到buf中
        */
       // 计算null_bitmap
       char null_bitmap = 0;
-      for (unsigned j = 0; j<8; j++) {
+      for (uint32_t j = 0; j<8; j++) {
         // 后续补0的情况
         if (i * 8 + j >= field_nums) {
           null_bitmap += 0;
@@ -133,7 +133,7 @@ class Row {
      */
     for (auto &field : fields_) {
       if (!field->IsNull()) {
-        offset += field->SerializeTo(buf + offset);
+        offset += field->Field::SerializeTo(buf + offset);
         // 在此处的SerializeTo()和GetSerializedSize()函数的返回值应该是一样的
         // 这里方便起见采用SerializeTo()进行返回
       }
@@ -141,7 +141,35 @@ class Row {
     return offset;
   }
 
-  uint32_t DeserializeFrom(char *buf, Schema *schema);
+  uint32_t DeserializeFrom(char *buf, Schema *schema) {
+    ASSERT(schema != nullptr, "Invalid Schema");
+    ASSERT(fields_.empty(), "Invalid field");
+    uint32_t offset = 0;  // 定义偏移量
+    size_t field_nums = MACH_READ_UINT32(buf);
+    offset = sizeof(field_nums);
+    uint32_t bitmap_num = (field_nums + 7) / 8;
+    std::vector<char> null_bitmaps;
+    // 读取所有的null_bitmap
+    for (uint32_t i = 0; i < bitmap_num; i++) {
+      char null_bitmap = MACH_READ_FROM(char, buf + offset);
+      offset += sizeof(char);
+      null_bitmaps[i] = null_bitmap;
+    }
+    // 将field内容插入field_后面
+    for (uint32_t i = 0; i < bitmap_num; i++) {
+      for (uint32_t j = 0; j<8; j++) {
+        // 首先，我们先要知道这一个要解决的field的类型
+        TypeId field_type = schema->GetColumn(i * 8 + j)->GetType();
+        // 我们使用二进制来解决
+        // 例如：| 0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |
+        std::bitset<10> bit(null_bitmaps[i]);
+        Field *field = nullptr;
+        offset += Field::DeserializeFrom(buf + offset, field_type, &field, bit[j] == 0);
+        fields_.push_back(field);
+      }
+    }
+    return offset;
+  }
 
   /**
    * For empty row, return 0
