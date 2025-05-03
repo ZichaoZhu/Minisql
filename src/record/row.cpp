fd7b77f0dc3c06072799bd8466adec3d87a6b588
ZichaoZhu <1606783699@qq.com>
2025-05-03 14:37:10 +0800

    对row.cpp文件修改并通过测试

--- src/record/row.cpp
+++ src/record/row.cpp
@@ -17,7 +17,7 @@ uint32_t Row::SerializeTo(char *buf, Schema *schema) const {
   // replace with your code here
   uint32_t offset = 0;  // 定义偏移量
   size_t field_nums = fields_.size(); // 得到字段个数
-  MACH_WRITE_UINT32(buf, field_nums); // 写入字段个数
+  MACH_WRITE_TO(size_t, buf, field_nums); // 写入字段个数
   offset = sizeof(field_nums);
   /* 对于Null bitmap，我们考虑采用多个char类型变量进行序列化
    * 简单来说，就是每个char类型变量的每一位代表一个字段是否为空
@@ -36,6 +36,7 @@ uint32_t Row::SerializeTo(char *buf, Schema *schema) const {
     char null_bitmap = 0;
     for (uint32_t j = 0; j<8; j++) {
       // 后续补0的情况
+      null_bitmap <<= 1; // 左移一位
       if (i * 8 + j >= field_nums) {
         null_bitmap += 0;
       }
@@ -47,7 +48,6 @@ uint32_t Row::SerializeTo(char *buf, Schema *schema) const {
           null_bitmap += 1;
         }
       }
-      null_bitmap <<= 1; // 左移一位
     }
     // 存储到buf中
     MACH_WRITE_TO(char, buf + offset, null_bitmap);
@@ -73,26 +73,26 @@ uint32_t Row::DeserializeFrom(char *buf, Schema *schema) {
   ASSERT(fields_.empty(), "Non empty field in row.");
   // replace with your code here
   uint32_t offset = 0;  // 定义偏移量
-  size_t field_nums = MACH_READ_UINT32(buf);
-  offset = sizeof(field_nums);
+  size_t field_nums = MACH_READ_FROM(size_t, buf);  // 需要注意这里的类型
+  offset = sizeof(size_t);
   uint32_t bitmap_num = (field_nums + 7) / 8;
   std::vector<char> null_bitmaps;
   // 读取所有的null_bitmap
   for (uint32_t i = 0; i < bitmap_num; i++) {
     char null_bitmap = MACH_READ_FROM(char, buf + offset);
     offset += sizeof(char);
-    null_bitmaps[i] = null_bitmap;
+    null_bitmaps.push_back(null_bitmap);
   }
   // 将field内容插入field_后面
   for (uint32_t i = 0; i < bitmap_num; i++) {
-    for (uint32_t j = 0; j<8; j++) {
+    std::bitset<8> bit(null_bitmaps[i]);
+    for (uint32_t j = 0; j < 8 && i * 8 + j < field_nums; j++) {
       // 首先，我们先要知道这一个要解决的field的类型
       TypeId field_type = schema->GetColumn(i * 8 + j)->GetType();
       // 我们使用二进制来解决
       // 例如：| 0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |
-      std::bitset<10> bit(null_bitmaps[i]);
       Field *field = nullptr;
-      offset += Field::DeserializeFrom(buf + offset, field_type, &field, bit[j] == 0);
+      offset += Field::DeserializeFrom(buf + offset, field_type, &field, !bit[8 - 1 - j]);
       fields_.push_back(field);
     }
   }
