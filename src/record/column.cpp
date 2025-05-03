fd5df10bcf58687328a9d9acb42a9194e9d483f1
ZichaoZhu <1606783699@qq.com>
2025-05-03 10:38:56 +0800

    在column.cpp中补全SerializeTo()函数

--- src/record/column.cpp
+++ src/record/column.cpp
@@ -40,7 +40,37 @@ Column::Column(const Column *other)
 */
 uint32_t Column::SerializeTo(char *buf) const {
   // replace with your code here
-  return 0;
+  /*我们采用以下格式进行序列化：
+   * | COLUMN_MAGIC_NUM | name_len | name | type | len | table_ind | nullable | unique |
+   * 因为name是string类型，因此我们需要记录其长度
+   * 幸运的是，string.length()是size_t类型的，是定长的
+   */
+  uint32_t offset = 0;  // 初始化偏移量
+  // 序列化COLUMN_MAGIC_NUM
+  MACH_WRITE_UINT32(buf, COLUMN_MAGIC_NUM);
+  offset += sizeof(uint32_t);
+  // 序列化name_len以及name
+  size_t name_len = name_.length();
+  MACH_WRITE_UINT32(buf + offset, name_len);
+  offset += sizeof(uint32_t);
+  MACH_WRITE_STRING(buf + offset, name_);
+  offset += name_len;
+  // 序列化type
+  MACH_WRITE_TO(TypeId, buf + offset, type_);
+  offset += sizeof(type_);
+  // 序列化len
+  MACH_WRITE_UINT32(buf + offset, len_);
+  offset += sizeof(uint32_t);
+  // 序列化table_ind
+  MACH_WRITE_UINT32(buf + offset, table_ind_);
+  offset += sizeof(uint32_t);
+  // 序列化nullable
+  MACH_WRITE_TO(bool, buf + offset, nullable_);
+  offset += sizeof(bool);
+  // 序列化unique
+  MACH_WRITE_TO(bool, buf + offset, unique_);
+  offset += sizeof(bool);
+  return offset;
 }
 
 /**
