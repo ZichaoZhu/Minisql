ef877295d276f9be3e0deaf1545466050e2c099b
ZichaoZhu <1606783699@qq.com>
2025-05-04 13:36:23 +0800

    完成table.cpp中的InsertTuple函数的编写

--- src/storage/table_heap.cpp
+++ src/storage/table_heap.cpp
@@ -3,7 +3,50 @@
 /**
  * TODO: Student Implement
  */
-bool TableHeap::InsertTuple(Row &row, Txn *txn) { return false; }
+bool TableHeap::InsertTuple(Row &row, Txn *txn) {
+  // 首先判断当前的row是否过大
+  if (row.GetSerializedSize(schema_) > TablePage::SIZE_MAX_ROW) {
+    return false;
+  }
+  // 获得第一个page
+  auto page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(GetFirstPageId()));
+  if (page == nullptr) {
+    return false;
+  }
+  page->WLatch();
+  while (!page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_)) {  // 若是无法插入
+    page->WUnlatch(); // 解锁
+    auto next_page_id = page->GetNextPageId();  // 获取下一个page_id
+    // 若next_page_id恰好为INVALID_PAGE_ID，则说明当前的page已经是最后一个page了，而且无法插入，此时我们需要新建一个page
+    if (next_page_id == INVALID_PAGE_ID) {
+      auto new_page = reinterpret_cast<TablePage *>(buffer_pool_manager_->NewPage(next_page_id));
+      if (new_page == nullptr) {
+        return false;
+      }
+      // 初始化新页
+      new_page->Init(next_page_id, page->GetTablePageId(), log_manager_, txn);
+      new_page->SetNextPageId(INVALID_PAGE_ID);
+      // 更新前面的页
+      page->SetNextPageId(next_page_id);
+      // unpin，同时由于修改了page，所以需要将其标记为dirty
+      buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
+      page = new_page;
+    }
+    // 若不相等，则不为最后一个page
+    else {
+      auto next_page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(next_page_id));
+      buffer_pool_manager_->UnpinPage(page->GetTablePageId(), false);
+      page = next_page;
+    }
+    // 这里需要加锁
+    page->WLatch();
+  }
+  // 插入成功，解锁
+  page->WUnlatch();
+  // 这里需要unpin
+  buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
+  return true;
+}
 
 bool TableHeap::MarkDelete(const RowId &rid, Txn *txn) {
   // Find the page which contains the tuple.
